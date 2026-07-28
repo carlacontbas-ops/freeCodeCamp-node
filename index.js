7a7b43e9afcd0c3276970e8a20110da9e6554910
Carla Contreras <carla.cont.bas@gmail.com>
2026-07-27 21:03:02 -0400

    Mostrar Hello World en la app

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require("express");
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-
 app.get("/", (req, res) => {
-  res.send("Hola, soy Carla Contreras. Mi proyecto Node.js funciona correctamente.");
+  console.log("Hello World");
+  res.send("Hello World");
 });
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Servidor funcionando en puerto ${PORT}`);
